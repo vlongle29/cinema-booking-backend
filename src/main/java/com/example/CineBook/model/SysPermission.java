917eba1361b4bc43c3541232096ef00911ae82f5
vlongle29 <longvis2004@gmail.com>
2025-11-25 23:24:27 +0700

    update(SysPermission)

--- src/main/java/com/example/CineBook/model/SysPermission.java
+++ src/main/java/com/example/CineBook/model/SysPermission.java
@@ -1,13 +1,18 @@
 package com.example.CineBook.model;
 
 import com.example.CineBook.model.auditing.AuditingEntity;
+import com.example.CineBook.model.auditing.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.metamodel.StaticMetamodel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@StaticMetamodel(SysPermission.class)
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
