4a9f54b2156ca8ab7cc8d9866659c710edf8a450
vlongle29 <longvis2004@gmail.com>
2025-12-02 21:37:19 +0700

    fix(employee): @EqualsAndHashCode(callSuper) -> @Getter, @Setter in searchEmployeeDTO

--- src/main/java/com/example/CineBook/dto/employee/EmployeeSearchDTO.java
+++ src/main/java/com/example/CineBook/dto/employee/EmployeeSearchDTO.java
@@ -1,11 +1,13 @@
 package com.example.CineBook.dto.employee;
 
 import com.example.CineBook.common.dto.request.SearchBaseDto;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 public class EmployeeSearchDTO extends SearchBaseDto {
     private String name;
     private String employeeCode;
