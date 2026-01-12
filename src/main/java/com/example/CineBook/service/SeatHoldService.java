1a7dd8a1ec773a7718b4ef6839c2298a0e5d3b41
vlongle29 <longvis2004@gmail.com>
2026-01-12 23:18:57 +0700

    fix(SeatHold): add getHoldsByBooking function.

--- src/main/java/com/example/CineBook/service/SeatHoldService.java
+++ src/main/java/com/example/CineBook/service/SeatHoldService.java
@@ -1,11 +1,14 @@
 package com.example.CineBook.service;
 
+import com.example.CineBook.dto.seat.SeatHoldData;
 import com.example.CineBook.dto.seat.SeatHoldRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SeatHoldService {
     void holdSeats(UUID bookingId, SeatHoldRequest request); // Hold 1 seat
     void releaseSeat(UUID bookingId, UUID showtimeId, UUID seatId); // Release 1 seat
     void releaseSeats(UUID bookingId); // Release all seats of booking
+    List<SeatHoldData> getHoldsByBooking(UUID bookingId);
 }
