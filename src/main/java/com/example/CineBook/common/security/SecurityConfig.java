953f1cc029660cf9fbcf31b9a3ca0acb563b882d
vlongle29 <longvis2004@gmail.com>
2026-05-06 01:05:45 +0700

    refactor(SecurityConfig): add url into PUBLIC_ENDPOINTS

--- src/main/java/com/example/CineBook/common/security/SecurityConfig.java
+++ src/main/java/com/example/CineBook/common/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
             "/api/showtimes/available-cities",
             "/api/showtimes/available-formats",
             "/api/showtimes/grouped-by-branch",
+            "/api/movies/search",
+            "/api/movies/**",
             "/api/payments/vn-pay-callback",
             // Thêm các dòng này để cho phép truy cập Swagger
             "/swagger-ui/**",
