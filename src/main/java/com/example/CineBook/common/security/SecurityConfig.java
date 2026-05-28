7b4db2f6f0679cdb21be4509c9b6801f05d36b86
vlongle29 <longvis2004@gmail.com>
2026-05-29 00:47:21 +0700

    fix(auth): fix PUBLIC_ENDPOINTS

--- src/main/java/com/example/CineBook/common/security/SecurityConfig.java
+++ src/main/java/com/example/CineBook/common/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
             "/api/showtimes/available-formats",
             "/api/showtimes/grouped-by-branch",
             "/api/movies/search",
-            "/api/movies/**",
+//            "/api/movies/**",
             "/api/payments/vn-pay-callback",
             // Thêm các dòng này để cho phép truy cập Swagger
             "/swagger-ui/**",
